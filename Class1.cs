8e531107f48e9e8aa1f1ac9da882c279eadca358
wcmsukwa <wcmsukwa@gmail.com>
2020-01-14 23:26:39 +0200

    Task: Adding statistical operations

--- Class1.cs
+++ Class1.cs
@@ -4,5 +4,6 @@ namespace CalculatorExam
 {
     public class Class1
     {
+                                                                                                                                                                                                                                                                                                                                        
     }
 }
