3cbec3317f24cc3f508f404dc5757a67f100d950
gmachalo <57180690+gmachalo@users.noreply.github.com>
2020-01-15 11:48:45 +0200

    Fixed: CalculatorExam

---
+++ BasicCalc/BasicCalculator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace BasicCalc
+{
+    public class BasicCalculator
+    {
+        private Adding add = new Adding();
+        private Div divide = new Div();
+        private Subtracting subtract = new Subtracting();
+        private Multiplying multiply = new Multiplying();
+        private Squaring square = new Squaring();
+        private Rooting root = new Rooting();
+        public dynamic result;
+
+        public dynamic Add(dynamic a, dynamic b)
+        {
+            result = add.Add(a, b);
+            return result;
+        }
+
+        public dynamic Add(dynamic values)
+        {
+            result = add.Add(values);
+            return result;
+        }
+
+        public dynamic Divide(dynamic a, dynamic b)
+        {
+            result = divide.Divide(a, b);
+            return result;
+        }
+
+        public dynamic Subtract(dynamic a, dynamic b)
+        {
+            result = subtract.Subtract(a, b);
+            return result;
+        }
+
+        public dynamic Multiply(dynamic a, dynamic b)
+        {
+            result = multiply.Multiply(a, b);
+            return result;
+        }
+
+        public dynamic SquareInterface(dynamic a)
+        {
+            result = square.SquareInterface(a);
+            return result;
+        }
+
+        public dynamic Root(dynamic a)
+        {
+            result = root.Root(a);
+            return result;
+        }
+    }
+}
---
+++ Ops/SquareRoot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ops
+{
+    public class SquareRoot
+    {
+        public static double Root(double a)
+        {
+            a = Math.Sqrt(25.0);
+            return a;
+        }
+    }
+}
