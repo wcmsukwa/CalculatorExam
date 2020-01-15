55e019eef48f32e6aa371d6ab45f6ad4dc3c0e21
wcmsukwa <wcmsukwa@gmail.com>
2020-01-15 10:19:20 +0200

    Delete Class1.cs

--- Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace CalculatorExam
-{
-    public class Class1
-    {
-                                                                                                                                                                                                                                                                                                                                        
-    }
-}
